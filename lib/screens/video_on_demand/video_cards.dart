864e336e80e847d4c0c041aeae1143552407aad1
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-04-02 18:41:48 +0530

    Splitted the vod screen into multiple files

---
+++ lib/screens/video_on_demand/video_cards.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:pocmytv/screens/video_on_demand/parallex_flow.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoCard extends StatefulWidget {
+  const VideoCard({
+    super.key,
+    required this.assetPath,
+    required this.isSelected,
+  });
+
+  final String assetPath;
+
+  final bool isSelected;
+
+  @override
+  State<VideoCard> createState() => _VideoCardState();
+}
+
+class _VideoCardState extends State<VideoCard> {
+  final GlobalKey _videoKey = GlobalKey();
+
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.assetPath);
+
+    _controller
+      ..addListener(() => setState(() {}))
+      ..setLooping(true)
+      ..setVolume(0)
+      ..initialize().then((_) => setState(() {}))
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 250),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Flow(
+          delegate: ParallaxFlowDelegate(
+            scrollable: Scrollable.of(context),
+            listItemContext: context,
+            backgroundImageKey: _videoKey,
+          ),
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(
+                  _controller,
+                  key: _videoKey,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
