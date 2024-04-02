a7fdd0f9877463f5031aba38e3e5b7b1afd244a3
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-04-02 18:31:03 +0530

    Fixed some UI issues

--- lib/focus_system/focus_service.dart
+++ lib/focus_system/focus_service.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:pocmytv/focus_system/focus_group.dart';
 
@@ -8,7 +6,7 @@ class FocusService {
   static String? _lastFocusedGroup;
 
   static void add(String group, FocusNode focusNode) {
-    log('Adding FocusNode to group $group', name: 'FocusService.add');
+    // log('Adding FocusNode to group $group', name: 'FocusService.add');
     if (!_focusGroups.containsKey(group)) {
       _focusGroups[group] = FocusGroup();
     }
@@ -16,25 +14,25 @@ class FocusService {
   }
 
   static void remove(String group, FocusNode focusNode) {
-    log('Removing FocusNode from group $group', name: 'FocusService.remove');
+    // log('Removing FocusNode from group $group', name: 'FocusService.remove');
     if (_focusGroups.containsKey(group)) {
       _focusGroups[group]!.remove(focusNode);
     } else {
-      log('FocusGroup $group not found', name: 'FocusService.remove');
+      // log('FocusGroup $group not found', name: 'FocusService.remove');
     }
   }
 
   static bool requestFocus(String group, FocusNode focusNode) {
-    log('Requesting FocusNode in group $group',
-        name: 'FocusService.requestFocus');
+    // log('Requesting FocusNode in group $group',
+    // name: 'FocusService.requestFocus')
     if (_lastFocusedGroup != group) {
-      log("Focus was changed to from group: $_lastFocusedGroup to group: $group",
-          name: "FocusService.requestFocus");
+      // log("Focus was changed to from group: $_lastFocusedGroup to group: $group",
+      // name: "FocusService.requestFocus")
       _lastFocusedGroup = group;
       _focusGroups[group]?.focus();
       return _focusGroups[group]?.lastFocusedNode == focusNode;
     }
-    log("Focus was not changed", name: "FocusService.requestFocus");
+    // log("Focus was not changed", name: "FocusService.requestFocus");
     _focusGroups[group]?.focusNode(focusNode);
     return true;
   }
