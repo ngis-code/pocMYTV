f65f3b07c8ec8d5916beaaf28b919dd029de5878
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-04-03 00:21:29 +0530

    New Update

--- lib/screens/notification/notification_screen.dart
+++ lib/screens/notification/notification_screen.dart
@@ -13,93 +13,285 @@ class NotificationScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Notifications',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Notifications',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      FocusWidget(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.close,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                FocusWidget(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Icon(
-                    Icons.close,
-                    color: Colors.white,
+                  FocusWidget(
+                    hasFocus: true,
+                    focusGroup: 'promotionalGroup',
+                    onTap: () {},
+                    child: const ListTile(
+                      leading: Icon(
+                        Icons.mail_outline_outlined,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        'AI Promotional Guest',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        '20% off on Gucci accessories only for you',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            FocusWidget(
-              hasFocus: true,
-              focusGroup: 'promotionalGroup',
-              onTap: () {},
-              child: const ListTile(
-                leading: Icon(
-                  Icons.mail_outline_outlined,
-                  color: Colors.white,
-                ),
-                title: Text(
-                  'AI Promotional Guest',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                  FocusWidget(
+                    focusGroup: 'promotionalGroup',
+                    onTap: () {},
+                    child: const ListTile(
+                      leading: Icon(
+                        Icons.mail_outline_outlined,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        'AI Promotional Guest',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        '20% off on Gucci accessories only for you',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                subtitle: Text(
-                  '20% off on Gucci accessories only for you',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                  ),
-                ),
+                  RatingBar(
+                      ratingWidget: RatingWidget(
+                        full: const Icon(Icons.star, color: Colors.amber),
+                        half: const Icon(Icons.star_half, color: Colors.amber),
+                        empty:
+                            const Icon(Icons.star_border, color: Colors.amber),
+                      ),
+                      initialRating: 3,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemSize: 30,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      onRatingUpdate: (rating) {}),
+                ],
               ),
             ),
-            FocusWidget(
-              focusGroup: 'promotionalGroup',
-              onTap: () {},
-              child: const ListTile(
-                leading: Icon(
-                  Icons.mail_outline_outlined,
-                  color: Colors.white,
-                ),
-                title: Text(
-                  'AI Promotional Guest',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: GridView.extent(
+                      maxCrossAxisExtent: 200,
+                      children: [
+                        // some promotional images and text
+                        Card(
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.transparent,
+                          child: GridTile(
+                            footer: const GridTileBar(
+                              backgroundColor: Colors.black45,
+                              title: Text(
+                                'AI Promotional Guest',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                '20% off on Gucci accessories only for you',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            child: Image.network(
+                              'https://incubator.ucf.edu/wp-content/uploads/2023/07/artificial-intelligence-new-technology-science-futuristic-abstract-human-brain-ai-technology-cpu-central-processor-unit-chipset-big-data-machine-learning-cyber-mind-domination-generative-ai-scaled-1.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Card(
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.transparent,
+                          child: GridTile(
+                            footer: const GridTileBar(
+                              backgroundColor: Colors.black45,
+                              title: Text(
+                                'AI Promotional Guest',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                '20% off on Gucci accessories only for you',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            child: Image.network(
+                              'https://incubator.ucf.edu/wp-content/uploads/2023/07/artificial-intelligence-new-technology-science-futuristic-abstract-human-brain-ai-technology-cpu-central-processor-unit-chipset-big-data-machine-learning-cyber-mind-domination-generative-ai-scaled-1.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Card(
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.transparent,
+                          child: GridTile(
+                            footer: const GridTileBar(
+                              backgroundColor: Colors.black45,
+                              title: Text(
+                                'AI Promotional Guest',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                '20% off on Gucci accessories only for you',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            child: Image.network(
+                              'https://incubator.ucf.edu/wp-content/uploads/2023/07/artificial-intelligence-new-technology-science-futuristic-abstract-human-brain-ai-technology-cpu-central-processor-unit-chipset-big-data-machine-learning-cyber-mind-domination-generative-ai-scaled-1.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Card(
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.transparent,
+                          child: GridTile(
+                            footer: const GridTileBar(
+                              backgroundColor: Colors.black45,
+                              title: Text(
+                                'AI Promotional Guest',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                '20% off on Gucci accessories only for you',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            child: Image.network(
+                              'https://incubator.ucf.edu/wp-content/uploads/2023/07/artificial-intelligence-new-technology-science-futuristic-abstract-human-brain-ai-technology-cpu-central-processor-unit-chipset-big-data-machine-learning-cyber-mind-domination-generative-ai-scaled-1.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Card(
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.transparent,
+                          child: GridTile(
+                            footer: const GridTileBar(
+                              backgroundColor: Colors.black45,
+                              title: Text(
+                                'AI Promotional Guest',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                '20% off on Gucci accessories only for you',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            child: Image.network(
+                              'https://incubator.ucf.edu/wp-content/uploads/2023/07/artificial-intelligence-new-technology-science-futuristic-abstract-human-brain-ai-technology-cpu-central-processor-unit-chipset-big-data-machine-learning-cyber-mind-domination-generative-ai-scaled-1.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Card(
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          color: Colors.transparent,
+                          child: GridTile(
+                            footer: const GridTileBar(
+                              backgroundColor: Colors.black45,
+                              title: Text(
+                                'AI Promotional Guest',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                '20% off on Gucci accessories only for you',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            child: Image.network(
+                              'https://incubator.ucf.edu/wp-content/uploads/2023/07/artificial-intelligence-new-technology-science-futuristic-abstract-human-brain-ai-technology-cpu-central-processor-unit-chipset-big-data-machine-learning-cyber-mind-domination-generative-ai-scaled-1.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                subtitle: Text(
-                  '20% off on Gucci accessories only for you',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
+                  const Expanded(
+                    child: Text('Right'),
                   ),
-                ),
+                ],
               ),
-            ),
-            RatingBar(
-                ratingWidget: RatingWidget(
-                  full: const Icon(Icons.star, color: Colors.amber),
-                  half: const Icon(Icons.star_half, color: Colors.amber),
-                  empty: const Icon(Icons.star_border, color: Colors.amber),
-                ),
-                initialRating: 3,
-                minRating: 1,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemSize: 30,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                onRatingUpdate: (rating) {}),
+            )
           ],
         ),
       ),
