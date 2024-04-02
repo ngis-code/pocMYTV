c0e75f1285fae385b9afe0ea0091364b4afd043d
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-04-02 21:39:04 +0530

    Home screen

--- lib/screens/home/home_tile.dart
+++ lib/screens/home/home_tile.dart
@@ -3,18 +3,18 @@ import 'package:pocmytv/utils/glass_widget.dart';
 
 class HomeTile extends StatelessWidget {
   final Widget child;
-  final EdgeInsets? padding;
-  const HomeTile({super.key, required this.child, this.padding});
+  final EdgeInsets padding;
+  const HomeTile(
+      {super.key,
+      required this.child,
+      this.padding = const EdgeInsets.all(20)});
 
   @override
   Widget build(BuildContext context) {
     return GlassWidget(
       blur: 20,
       radius: 20,
-      child: Container(
-          color: Colors.black45,
-          padding: padding ?? const EdgeInsets.all(20),
-          child: child),
+      child: Container(color: Colors.black45, padding: padding, child: child),
     );
   }
 }
