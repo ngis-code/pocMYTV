ac16673862b38d3321b754b335adf28665183bfd
raman04-byte <ramantank04022002@gmail.com>
2024-04-03 23:17:26 +0530

    Updation in safety_screen

--- lib/screens/safety/safety_screen.dart
+++ lib/screens/safety/safety_screen.dart
@@ -1,10 +1,22 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:pocmytv/screens/animation/bubble_animation.dart';
 import 'package:pocmytv/utils/glass_widget.dart';
 import 'package:pocmytv/widgets/drawer_row.dart';
 
-class SafetyScreen extends StatelessWidget {
+class SafetyScreen extends StatefulWidget {
   const SafetyScreen({super.key});
+
+  @override
+  State<SafetyScreen> createState() => _SafetyScreenState();
+}
+
+class _SafetyScreenState extends State<SafetyScreen> {
+  final player = AudioPlayer();
+
+  Future<void> playAudioFromUrl(String url) async {
+    await player.play(UrlSource(url));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +62,6 @@ class SafetyScreen extends StatelessWidget {
                     Expanded(
                       child: GlassWidget(
                         padding: const EdgeInsets.all(10),
-                        // backgroundColor: Colors.white,
                         radius: 20,
                         child: Container(
                           decoration: BoxDecoration(
@@ -66,9 +77,11 @@ class SafetyScreen extends StatelessWidget {
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
-                              const Placeholder(
-                                fallbackHeight: 400,
-                                fallbackWidth: 200,
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Image.network(
+                                  'https://deeplink.recruitpick.com/uploads/lifejacket_img',
+                                ),
                               ),
                               const Text(
                                 'Life Jacket Tutorial',
@@ -85,7 +98,9 @@ class SafetyScreen extends StatelessWidget {
                                 height: 40,
                               ),
                               ElevatedButton(
-                                onPressed: () {},
+                                onPressed: () async {
+                                  await player.stop();
+                                },
                                 style: ElevatedButton.styleFrom(
                                   backgroundColor: Colors.grey,
                                   shape: const RoundedRectangleBorder(
@@ -106,37 +121,124 @@ class SafetyScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                    const Expanded(
+                    Expanded(
                       child: GlassWidget(
-                        padding: EdgeInsets.all(10),
-                        backgroundColor: Colors.white,
+                        padding: const EdgeInsets.all(10),
                         radius: 20,
-                        child: Column(
-                          children: [
-                            Text(
-                              'Life Jacket Tutorial',
-                              style: TextStyle(
-                                fontSize: 20,
-                              ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.grey,
+                              width: 1,
                             ),
-                          ],
+                          ),
+                          padding: const EdgeInsets.only(
+                              left: 20, right: 20, top: 10, bottom: 10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Image.network(
+                                  'https://deeplink.recruitpick.com/uploads/lifejacket_img',
+                                ),
+                              ),
+                              const Text(
+                                'Life Jacket Tutorial',
+                                style:
+                                    TextStyle(fontSize: 20, color: Colors.blue),
+                              ),
+                              const Text(
+                                'Learn how to wear a life jacket properly',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 40,
+                              ),
+                              ElevatedButton(
+                                onPressed: () async {
+                                  await playAudioFromUrl(
+                                      "https://deeplink.recruitpick.com/uploads/LATEST_Emergency%20Signal.mp3");
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.grey,
+                                  shape: const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(1),
+                                    ),
+                                  ),
+                                ),
+                                child: Text(
+                                  'Watch the Tutorial'.toUpperCase(),
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                    const Expanded(
+                    Expanded(
                       child: GlassWidget(
-                        padding: EdgeInsets.all(10),
-                        backgroundColor: Colors.white,
+                        padding: const EdgeInsets.all(10),
                         radius: 20,
-                        child: Column(
-                          children: [
-                            Text(
-                              'Life Jacket Tutorial',
-                              style: TextStyle(
-                                fontSize: 20,
-                              ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.grey,
+                              width: 1,
                             ),
-                          ],
+                          ),
+                          padding: const EdgeInsets.only(
+                              left: 20, right: 20, top: 10, bottom: 10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Image.network(
+                                  'https://deeplink.recruitpick.com/uploads/IC_sign_B2.png'),
+                              const Text(
+                                'Your assembly station',
+                                style:
+                                    TextStyle(fontSize: 20, color: Colors.blue),
+                              ),
+                              const Text(
+                                'Go directly to your assembly station if you hear the emergency horn',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 40,
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.grey,
+                                  shape: const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(1),
+                                    ),
+                                  ),
+                                ),
+                                child: Text(
+                                  'find your station'.toUpperCase(),
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
