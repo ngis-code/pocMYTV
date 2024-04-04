e9dbd48e0eb667b08751c5a2cda2a8b77d4735f4
raman04-byte <ramantank04022002@gmail.com>
2024-04-04 21:28:17 +0530

    Completeion of the safety screen

--- lib/screens/safety/safety_screen.dart
+++ lib/screens/safety/safety_screen.dart
@@ -137,15 +137,14 @@ class _SafetyScreenState extends State<SafetyScreen> {
                                 FocusWidget(
                                   borderColor: Colors.black,
                                   onTap: () {
-                                    // Navigator.push(
-                                    //   context,
-                                    //   MaterialPageRoute(
-                                    //     builder: (context) => SafetyVideoScreen(
-                                    //       onVideoCompleted: videoCompleted,
-                                    //     ),
-                                    //   ),
-                                    // );
-                                    videoCompleted();
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => SafetyVideoScreen(
+                                          onVideoCompleted: videoCompleted,
+                                        ),
+                                      ),
+                                    );
                                   },
                                   child: Container(
                                     decoration: BoxDecoration(
@@ -216,10 +215,9 @@ class _SafetyScreenState extends State<SafetyScreen> {
                                       FocusWidget(
                                         borderColor: Colors.black,
                                         onTap: () async {
-                                          // await playAudioFromUrl(
-                                          //     "https://deeplink.recruitpick.com/uploads/LATEST_Emergency%20Signal.mp3");
+                                          await playAudioFromUrl(
+                                              "https://deeplink.recruitpick.com/uploads/LATEST_Emergency%20Signal.mp3");
                                           hornColorChange();
-                                          _isButton3Active = true;
                                         },
                                         child: Container(
                                           decoration: BoxDecoration(
@@ -317,13 +315,85 @@ class _SafetyScreenState extends State<SafetyScreen> {
                             ),
                     ),
                     Expanded(
-                        child: _isButton3Active
-                            ? Material(
-                                elevation: 5,
-                                color: Colors.transparent,
-                                borderRadius: BorderRadius.circular(20),
-                                child: GlassWidget(
-                                  padding: const EdgeInsets.all(10),
+                      child: _isButton3Active
+                          ? Material(
+                              elevation: 5,
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(20),
+                              child: GlassWidget(
+                                padding: const EdgeInsets.all(10),
+                                radius: 20,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.white,
+                                    border: Border.all(
+                                      color: Colors.grey,
+                                      width: 1,
+                                    ),
+                                  ),
+                                  padding: const EdgeInsets.only(
+                                      left: 20, right: 20, top: 10, bottom: 10),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Image.network(
+                                        'https://deeplink.recruitpick.com/uploads/IC_sign_B2.png',
+                                      ),
+                                      const Text(
+                                        'Your assembly station',
+                                        style: TextStyle(
+                                            fontSize: 20, color: Colors.blue),
+                                      ),
+                                      const Text(
+                                        'Go directly to your assembly\nstation if you hear the emergency horn',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 40,
+                                      ),
+                                      FocusWidget(
+                                        borderColor: Colors.black,
+                                        onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const SafetyImage(),
+                                            ),
+                                          );
+                                          safetyColorChange();
+                                        },
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            color: safetyColor,
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                          ),
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 5, horizontal: 10),
+                                          child: Text(
+                                            'find your station'.toUpperCase(),
+                                            style: const TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            )
+                          : Stack(
+                              children: [
+                                GlassWidget(
+                                  // padding: const EdgeInsets.all(10),
                                   radius: 20,
                                   child: Container(
                                     decoration: BoxDecoration(
@@ -361,32 +431,19 @@ class _SafetyScreenState extends State<SafetyScreen> {
                                         const SizedBox(
                                           height: 40,
                                         ),
-                                        FocusWidget(
-                                          borderColor: Colors.black,
-                                          onTap: () {
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                builder: (context) =>
-                                                    const SafetyImage(),
-                                              ),
-                                            );
-                                            safetyColorChange();
-                                          },
-                                          child: Container(
-                                            decoration: BoxDecoration(
-                                              color: safetyColor,
-                                              borderRadius:
-                                                  BorderRadius.circular(20),
-                                            ),
-                                            padding: const EdgeInsets.symmetric(
-                                                vertical: 5, horizontal: 10),
-                                            child: Text(
-                                              'find your station'.toUpperCase(),
-                                              style: const TextStyle(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.bold,
-                                              ),
+                                        Container(
+                                          decoration: BoxDecoration(
+                                            color: Colors.grey,
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                          ),
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 5, horizontal: 10),
+                                          child: Text(
+                                            'find your station'.toUpperCase(),
+                                            style: const TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
                                             ),
                                           ),
                                         ),
@@ -394,79 +451,18 @@ class _SafetyScreenState extends State<SafetyScreen> {
                                     ),
                                   ),
                                 ),
-                              )
-                            : Stack(
-                                children: [
-                                  GlassWidget(
-                                    // padding: const EdgeInsets.all(10),
-                                    radius: 20,
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(20),
-                                        color: Colors.white,
-                                        border: Border.all(
-                                          color: Colors.grey,
-                                          width: 1,
-                                        ),
-                                      ),
-                                      padding: const EdgeInsets.only(
-                                          left: 20,
-                                          right: 20,
-                                          top: 10,
-                                          bottom: 10),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceEvenly,
-                                        children: [
-                                          Image.network(
-                                            'https://deeplink.recruitpick.com/uploads/IC_sign_B2.png',
-                                          ),
-                                          const Text(
-                                            'Your assembly station',
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                color: Colors.blue),
-                                          ),
-                                          const Text(
-                                            'Go directly to your assembly\nstation if you hear the emergency horn',
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(
-                                              fontSize: 15,
-                                            ),
-                                          ),
-                                          const SizedBox(
-                                            height: 40,
-                                          ),
-                                          Container(
-                                            decoration: BoxDecoration(
-                                              color: Colors.grey,
-                                              borderRadius:
-                                                  BorderRadius.circular(20),
-                                            ),
-                                            padding: const EdgeInsets.symmetric(
-                                                vertical: 5, horizontal: 10),
-                                            child: Text(
-                                              'find your station'.toUpperCase(),
-                                              style: const TextStyle(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  Positioned.fill(
-                                      child: GlassWidget(
+                                Positioned.fill(
+                                  child: GlassWidget(
                                     blur: 1,
                                     backgroundColor:
                                         Colors.grey.withOpacity(0.5),
                                     radius: 20,
                                     child: Container(),
-                                  ))
-                                ],
-                              )),
+                                  ),
+                                ),
+                              ],
+                            ),
+                    ),
                   ],
                 ),
               ),
