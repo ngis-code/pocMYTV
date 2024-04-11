886773ff2dbe893aff90e6b54151e679b63393f9
raman04-byte <ramantank04022002@gmail.com>
2024-04-12 01:01:00 +0530

    New Update

--- start_json_server.sh
+++ start_json_server.sh
@@ -1,6 +1,6 @@
 npm install -g json-server
 clear
 echo Below is the current ip addr
-ip addr show
+ipconfig getifaddr en0
 echo change the ip addr and port to 3000 in globals.dart file
 npx json-server db.json
