68e3f2ceb419f32b0eee6c84f249ea401a0ce2ea
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-04-15 20:31:49 +0530

    fixed background stopping abruptly

--- lib/screens/background.dart/background_video.dart
+++ lib/screens/background.dart/background_video.dart
@@ -1,7 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:pocmytv/screens/main_page.dart';
+import 'dart:developer';
 
-class BackgroundVideo extends StatelessWidget {
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:pocmytv/focus_system/focus_widget.dart';
+import 'package:video_player/video_player.dart';
+
+class BackgroundVideo extends StatefulWidget {
   final Widget child;
   final bool showBackButton;
   final Widget? backgroundWidget;
@@ -13,15 +17,62 @@ class BackgroundVideo extends StatelessWidget {
   });
 
   @override
+  State<BackgroundVideo> createState() => _BackgroundVideoState();
+}
+
+class _BackgroundVideoState extends State<BackgroundVideo> {
+  late final VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+      kIsWeb
+          ? 'https://deeplink.recruitpick.com/uploads/bgMovie.mp4'
+          : 'https://deeplink.recruitpick.com/uploads/bg.avi',
+      videoPlayerOptions: VideoPlayerOptions(
+        mixWithOthers: true,
+        allowBackgroundPlayback: true,
+      ),
+    );
+    _controller
+        .initialize()
+        .then((_) => setState(() {
+              _controller
+                ..addListener(() => setState(() {}))
+                ..setLooping(true)
+                ..setVolume(0)
+                ..play();
+              setState(() {});
+            }))
+        .onError((error, stackTrace) => log("Error Loading Video: $error"));
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return SizedBox(
       width: double.infinity,
       height: double.infinity,
       child: Stack(
         children: [
-          Positioned.fill(child: backgroundWidget ?? MainPage.backgroundVideo),
-          Positioned.fill(child: child),
-          if (showBackButton)
+          Positioned.fill(
+              child: widget.backgroundWidget ??
+                  AspectRatio(
+                    aspectRatio: width / height,
+                    child: FocusWidget(
+                      borderColor: Colors.transparent,
+                      borderRadius: 0,
+                      blur: 0,
+                      borderWidth: 0,
+                      skipTraversal: true,
+                      onTap: () {},
+                      child: VideoPlayer(_controller),
+                    ),
+                  )),
+          Positioned.fill(child: widget.child),
+          if (widget.showBackButton && Navigator.of(context).canPop())
             Positioned(
               top: 10,
               left: 10,
--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,88 +1,24 @@
-import 'dart:developer';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:pocmytv/focus_system/focus_widget.dart';
-import 'package:pocmytv/pre_loader/pre_loader.dart';
 import 'package:pocmytv/screens/background.dart/background_video.dart';
 import 'package:pocmytv/screens/home/home_bottom_tiles.dart';
 import 'package:pocmytv/screens/home/home_page.dart';
-import 'package:video_player/video_player.dart';
 
-class MainPage extends StatefulWidget {
-  static Widget backgroundVideo = Container(color: Colors.blue);
+class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  late VideoPlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      PreLoader.load(context);
-    });
-    _controller = VideoPlayerController.network(
-      kIsWeb
-          ? 'https://deeplink.recruitpick.com/uploads/bgMovie.mp4'
-          : 'https://deeplink.recruitpick.com/uploads/bg.avi',
-      videoPlayerOptions: VideoPlayerOptions(
-        mixWithOthers: true,
-        allowBackgroundPlayback: true,
-      ),
-    );
-    _controller
-        .initialize()
-        .then((_) => setState(() {
-              _controller
-                ..addListener(() => setState(() {}))
-                ..setLooping(true)
-                ..setVolume(0)
-                ..play();
-              final width = MediaQuery.of(context).size.width;
-              final height = MediaQuery.of(context).size.height;
-              setState(() {
-                MainPage.backgroundVideo = AspectRatio(
-                  aspectRatio: width / height,
-                  child: FocusWidget(
-                    borderColor: Colors.transparent,
-                    borderRadius: 0,
-                    blur: 0,
-                    borderWidth: 0,
-                    skipTraversal: true,
-                    onTap: () {},
-                    child: VideoPlayer(_controller),
-                  ),
-                );
-              });
-            }))
-        .onError((error, stackTrace) => log("Error Loading Video: $error"));
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.transparent,
       body: BackgroundVideo(
-        backgroundWidget: MainPage.backgroundVideo,
         showBackButton: false,
-        child: const Padding(
+        child: Padding(
           padding: EdgeInsets.only(bottom: 20.0),
           child: HomePage(),
         ),
       ),
       extendBody: true,
-      bottomNavigationBar: const HomeBottomTiles(),
+      bottomNavigationBar: HomeBottomTiles(),
     );
   }
 }
