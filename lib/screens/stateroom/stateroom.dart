454fa759ecfbaa9bc71314cf4a08099029ea3ee9
raman04-byte <ramantank04022002@gmail.com>
2024-04-16 18:15:25 +0530

    New Update

---
+++ lib/screens/stateroom/stateroom.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StateRoom extends StatefulWidget {
+  const StateRoom({super.key});
+
+  @override
+  State<StateRoom> createState() => _StateRoomState();
+}
+
+class _StateRoomState extends State<StateRoom> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
