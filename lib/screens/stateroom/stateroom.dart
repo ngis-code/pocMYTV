ec0747b46498b45507970d9be68d91a71329574d
raman04-byte <ramantank04022002@gmail.com>
2024-04-18 01:29:23 +0530

    New Update

--- lib/screens/stateroom/stateroom.dart
+++ lib/screens/stateroom/stateroom.dart
@@ -22,6 +22,7 @@ class _StateRoomState extends State<StateRoom> {
   // !!Even for something for the Stateroom Automation
   // !!Animation in the homepage with the ship
   // !!Bottom Tile Indicator (Floating Bottom Tile Indicator)
+  // !!Fixes in the Tile Indicator
 
   int acTemp = 28;
   bool curtainSwitch = false;
