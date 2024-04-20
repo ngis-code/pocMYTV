399a255be94e987b1f8d9eb6f3bda82636a84765
raman04-byte <ramantank04022002@gmail.com>
2024-04-20 19:06:07 +0530

    Updation in app.dart for the fix orientation of the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pocmytv/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
