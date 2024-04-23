30d632ed637cd7a132b24669413a96b51c0beb8f
raman04-byte <ramantank04022002@gmail.com>
2024-04-23 19:40:39 +0530

    Updation in orientation

--- lib/models/bottom_menu_model.dart
+++ lib/models/bottom_menu_model.dart
@@ -1,45 +1,45 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class BottomMenuModel {
-  String title;
-  String description;
-  IconData icon;
+// class BottomMenuModel {
+//   String title;
+//   String description;
+//   IconData icon;
 
-  BottomMenuModel({
-    required this.title,
-    required this.description,
-    required this.icon,
-  });
-  static List<BottomMenuModel> allBottomMenu = [
-    BottomMenuModel(
-      title: "VOD",
-      description: "Watch your favorite show",
-      icon: Icons.tv_rounded,
-    ),
-    BottomMenuModel(
-      title: "Schedule",
-      description: "Cruise Schedule",
-      icon: Icons.timeline,
-    ),
-    BottomMenuModel(
-      title: "Hotel Info",
-      description: "Know about the hotel",
-      icon: Icons.hotel_rounded,
-    ),
-    BottomMenuModel(
-      title: "Weather",
-      description: "Check the weather",
-      icon: Icons.cloud_rounded,
-    ),
-    BottomMenuModel(
-      title: "Restaurant & Bars",
-      description: "Browse your favorite place",
-      icon: Icons.restaurant_rounded,
-    ),
-    BottomMenuModel(
-      title: "News",
-      description: "Don't Miss Any News",
-      icon: Icons.newspaper_rounded,
-    ),
-  ];
-}
+//   BottomMenuModel({
+//     required this.title,
+//     required this.description,
+//     required this.icon,
+//   });
+//   static List<BottomMenuModel> allBottomMenu = [
+//     BottomMenuModel(
+//       title: "VOD",
+//       description: "Watch your favorite show",
+//       icon: Icons.tv_rounded,
+//     ),
+//     BottomMenuModel(
+//       title: "Schedule",
+//       description: "Cruise Schedule",
+//       icon: Icons.timeline,
+//     ),
+//     BottomMenuModel(
+//       title: "Hotel Info",
+//       description: "Know about the hotel",
+//       icon: Icons.hotel_rounded,
+//     ),
+//     BottomMenuModel(
+//       title: "Weather",
+//       description: "Check the weather",
+//       icon: Icons.cloud_rounded,
+//     ),
+//     BottomMenuModel(
+//       title: "Restaurant & Bars",
+//       description: "Browse your favorite place",
+//       icon: Icons.restaurant_rounded,
+//     ),
+//     BottomMenuModel(
+//       title: "News",
+//       description: "Don't Miss Any News",
+//       icon: Icons.newspaper_rounded,
+//     ),
+//   ];
+// }
