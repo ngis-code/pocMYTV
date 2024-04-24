b82d216437c029191f763b67fd7985eea0ec560d
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-04-24 23:10:58 +0530

    added an error msg in shipx screen

--- lib/screens/shorex/matter_port_3d_view.dart
+++ lib/screens/shorex/matter_port_3d_view.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:pocmytv/extensions/string_extensions.dart';
 import 'package:pocmytv/screens/background.dart/background_video.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -12,31 +13,43 @@ class ShipXScreen extends StatefulWidget {
 }
 
 class _ShipXScreenState extends State<ShipXScreen> {
-  final WebViewController _controller = WebViewController();
+  late final WebViewController _controller;
+  String error = "";
 
   @override
   void initState() {
     super.initState();
-    _controller.loadRequest(Uri.parse(
-        'https://my.matterport.com/show/?play=1&share=0&m=h1MEqdqBkXq'));
+    try {
+      _controller = WebViewController();
+      _controller.loadRequest(Uri.parse(
+          'https://my.matterport.com/show/?play=1&share=0&m=h1MEqdqBkXq'));
+    } catch (e) {
+      setState(() {
+        error = e.toString();
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: BackgroundVideo(
         backgroundWidget: Container(),
         child: SizedBox(
           width: double.infinity,
           height: double.infinity,
-          child: Platform.isAndroid || Platform.isIOS
+          child: error.isEmpty
               ? WebViewWidget(controller: _controller)
-              : Text(
-                  'Webview not supported on this platform. Please use Android or iOS.',
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color: Colors.red,
-                        fontWeight: FontWeight.bold,
-                      ),
+              : Center(
+                  child: Text(
+                    'Webview is not supported on ${Platform.operatingSystem.toPascalCase()}.\nUse the iOS app to view the content.\n\nWe are working on this and this view will be available soon.',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
                 ),
         ),
       ),
